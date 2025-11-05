374274c0ba24978025d9b1f80dbc5bf0dab6b735
Kailane Lisley <108088775+KailaneLisley@users.noreply.github.com>
2025-11-05 12:49:10 +0000

    Ajuste de backgroud (login e singup)

--- adapt/components/Footer/Footer.js
+++ adapt/components/Footer/Footer.js
@@ -2,16 +2,16 @@ import styles from './Footer.module.css';
 
 export function Footer() {
     return (
-   
-   <footer className={styles.footer}>
+      <footer className={styles.footer}>
         <div className={styles.footerContent}>
           <p>© 2025 Adapt, Inc.</p>
           <nav>
-            <a href="#">Quem Somos?</a>
+            <a href="/">Início</a>
+            <a href="/sobre">Quem Somos?</a>
             <a href="#">Parcerias</a>
             <a href="#">Termos & Privacidade</a>
           </nav>
         </div>
-    </footer>
+      </footer>
     );
 }  
