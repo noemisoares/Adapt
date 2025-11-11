d4221a7e3da3da978b915105804f8918321be087
Diogogeo27 <153551119+Diogogeo27@users.noreply.github.com>
2025-11-11 18:34:09 +0000

    melhorando a página boa práticas em desenvolvimento

--- adapt/app/dashboard/praticas/page.js
+++ adapt/app/dashboard/praticas/page.js
@@ -1,11 +1,28 @@
+"use client";
+
+import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function pratica() {
+export default function BoasPraticasPage() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h2>Pagina em desenvolvimento</h2>
-      </main>
-    </div>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.imageWrapper}>
+          <Image
+            src="/IdentidadeVisual/meioADAPT.png"
+            alt="Logo Adapt"
+            width={300}
+            height={150}
+            priority
+          />
+        </div>
+
+        <h1 className={styles.title}>Página em Desenvolvimento</h1>
+        <p className={styles.text}>
+          Em breve, você encontrará aqui boas práticas e orientações para criar
+          provas adaptadas de forma eficiente e inclusiva. 🚀
+        </p>
+      </div>
+    </main>
   );
 }
