cd7d8452e4cf7d564131e069b7ca4a3ed2be8536
christianlucas029 <lucasntc29@gmail.com>
2025-12-02 15:17:01 -0300

    teste

--- adapt/app/back4app/provas/getProvasUser.js
+++ adapt/app/back4app/provas/getProvasUser.js
@@ -22,13 +22,15 @@ export async function getProvasUser({ limit = 50, order = "-createdAt" } = {}) {
     titulo: p.get("titulo") || null,
     criadoEm: p.createdAt,
 
+    arquivoUrl: p.get("arquivoOriginal")?.url() || null,
+
     arquivoAdaptadoUrl: p.get("arquivoAdaptadoURL") || null, //
 
     //arquivoAdaptadoUrl: p.get("arquivoAdaptadoURL")?.url() || null, //
     //arquivoAdaptadoName: p.get("arquivoAdaptadoURL")?.name() || null, //
     adaptedText: p.get("adaptedText") || null,
 
-    arquivoUrl: p.get("arquivoOriginal")?.url() || null,
+    //arquivoUrl: p.get("arquivoOriginal")?.url() || null,
     //arquivoName: p.get("arquivoOriginal")?.name() || null,
   }));
 }
